01cd5d8b54a6f765d0e145c437ced2b0e5cf26be
doyindev1 <ade.doyin27@gmail.com>
2025-08-17 06:11:02 +0100

    Update layout to use defined local font variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { euclidCircularB } from "./styles";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Myxellia",
-  description: "Myxellia",
+  description: "Myxellia Assessment",
 };
 
 export default function RootLayout({
@@ -24,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${euclidCircularB.variable}`}>
         {children}
       </body>
     </html>
